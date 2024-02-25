ca5ec027aed1b6579fba00eaf2153fefa1c2d34c
odakris <christopher_denis@live.fr>
2024-02-25 22:17:40 +0100

    undo changes

--- server.js
+++ server.js
@@ -65,20 +65,20 @@ app.use(function (req, res, next) {
   res.status(404).type("text").send("Not Found");
 });
 
-//Start our server and tests!
-// const listener = app.listen(process.env.PORT || 3000, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-//   if (process.env.NODE_ENV === "test") {
-//     console.log("Running Tests...");
-//     setTimeout(function () {
-//       try {
-//         runner.run();
-//       } catch (e) {
-//         console.log("Tests are not valid:");
-//         console.error(e);
-//       }
-//     }, 1500);
-//   }
-// });
+// Start our server and tests!
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (e) {
+        console.log("Tests are not valid:");
+        console.error(e);
+      }
+    }, 1500);
+  }
+});
 
-// module.exports = app; //for testing
+module.exports = app; //for testing
